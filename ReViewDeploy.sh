af93460fa42720ea40db589398031aacdaa1ba45
wami <wamiwami@live.jp>
2018-05-09 00:17:34 +0900

    バッチファイル更新

--- ReViewDeploy.sh
+++ ReViewDeploy.sh
@@ -1,15 +1,17 @@
 #!/bin/sh
 
-git clone git@github.com:Nefry-Community/NefryLibraryDocumentReView.git
+git clone https://github.com/Nefry-Community/NefryLibraryDocumentReView.git
 
-# gitユーザ設定
-git config --global user.email "wamiwami@live.jp" #コミットユーザの有効なメールアドレス
-git config --global user.name "wami" #コミットユーザの名前
-#変更ファイルををすべて追加
-git add --all :/
-#コミット
-git commit -m "[auto] test"
-#リモートにプッシュ
-git push ${CI_REMOTE_REPOSITORY} ${CI_RELEASE_BRANCH}
-echo 
-echo "Success!"
+cp *.md NefryLibraryDocumentReView/articles/
+cp pic/* NefryLibraryDocumentReView/articles/images/
+
+cd NefryLibraryDocumentReView
+
+git remote set-url origin git@github.com:Nefry-Community/NefryLibraryDocumentReView.git
+
+git config --global user.email "wamiwami@live.jp"
+git config --global user.name "wami"
+
+git add --all
+git commit -m "[auto] commit LibraryDocument"
+git push origin master
